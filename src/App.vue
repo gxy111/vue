88ab4e1dd57e58322be1995461319fc8a79eff3d
gxy111 <1329935210@qq.com>
2019-02-22 12:09:48 +0800

    init my xiangmu

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="app-container">
+<mt-header fixed title="本地项目"></mt-header>
+<mt-tabbar v-model="selected">
+  <mt-tab-item id="tab1">
+    <img slot="icon" src="">
+    tab1
+  </mt-tab-item>
+  <mt-tab-item id="tab2">
+    <img slot="icon" src="">
+    tab2
+  </mt-tab-item>
+  <mt-tab-item id="tab3">
+    <img slot="icon" src="">
+    tab3
+  </mt-tab-item>
+  <mt-tab-item id="tab4">
+    <img slot="icon" src="">
+    tab4
+  </mt-tab-item>
+</mt-tabbar> 
+</div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
